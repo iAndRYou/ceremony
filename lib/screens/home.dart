98f102f9f81da80ca09fa7aa9a9c84480f1ecfd2
iAndRYou <nawet3razy@gmail.com>
2022-08-23 19:40:57 +0200

    AboutPage

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
       slivers: [
         SliverAppBar(
           floating: false,
-          pinned: false,
+          pinned: true,
           centerTitle: true,
           title: Text(
             "e-Legitymacja",
