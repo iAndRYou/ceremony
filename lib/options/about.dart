8f04dc5fd9e23f8bd183b8108281504e373b6d85
iAndRYou <nawet3razy@gmail.com>
2022-08-24 19:29:54 +0200

    Cleaning

--- lib/options/about.dart
+++ lib/options/about.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:introduction_screen/introduction_screen.dart';
