0fc2f6c8867cb3c31926e1c79374b8d71080ff78
iAndRYou <akmiecik@student.agh.edu.pl>
2022-09-19 13:29:31 +0200

    New Package

---
+++ android/app/src/main/java/com/ceremony/app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ceremony.ceremony;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/ceremony/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/ceremony/app/MainActivity.kt
@@ -3,5 +3,5 @@ package com.ceremony.app
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    
+
 }
